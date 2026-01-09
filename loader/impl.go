18ac6cc38f2b371376db8f3ef096bda123c071af
Alex IANCU <alexandru.iancu@gmail.com>
2026-01-09 19:12:19 +0200

    feat: adding metric provider and first instrument (I)

--- loader/impl.go
+++ loader/impl.go
@@ -1,8 +1,17 @@
 package loader
 
 import (
+	"context"
 	"fmt"
 	"log"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/metric"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	"spending/bldrec"
 	"spending/common"
@@ -10,6 +19,68 @@ import (
 	capnp "capnproto.org/go/capnp/v3"
 	zmq "github.com/pebbe/zmq4"
 )
+
+var glblMeterProvider *sdkmetric.MeterProvider
+
+// Initialize a gRPC connection to be used by both the tracer and meter
+// providers.
+func initConn() (*grpc.ClientConn, error) {
+	// It connects the OpenTelemetry Collector through local gRPC connection.
+	// You may replace `localhost:4317` with your endpoint.
+	conn, err := grpc.NewClient("localhost:4317",
+		// Note the use of insecure transport here. TLS is recommended in production.
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create gRPC connection to collector: %w", err)
+	}
+
+	return conn, err
+}
+
+func CreateMetricsPipeline(ctx context.Context) error {
+
+	comm, err := initConn()
+	if err != nil {
+		log.Fatalf("failed to initialize gRPC connection: %v", err)
+
+		return nil
+	}
+	// ---- OTLP gRPC exporter -------------------------------------------------
+	// Adjust the endpoint as needed (default: localhost:4317)
+	otlpExp, err := otlpmetricgrpc.New(ctx, otlpmetricgrpc.WithGRPCConn(comm))
+	if err != nil {
+		log.Fatalf("failed to create OTLP exporter: %v", err)
+
+		return nil
+	}
+
+	glblMeterProvider = sdkmetric.NewMeterProvider(sdkmetric.WithReader(sdkmetric.NewPeriodicReader(otlpExp)))
+	otel.SetMeterProvider(glblMeterProvider)
+	return nil
+}
+
+func CreateDebitInstrument() metric.Float64Histogram {
+
+	// ---- Create the histogram instrument ------------------------------------
+	meter := glblMeterProvider.Meter("spending/loader")
+	hist, err := meter.Float64Histogram(
+		"op.debit",
+		metric.WithUnit("ron"),
+		metric.WithDescription("bank account debit in currency RON"),
+	)
+	if err != nil {
+		log.Fatalf("failed to create histogram: %v", err)
+	}
+
+	return hist
+}
+
+func ShutdownMetric(ctx context.Context) {
+	if err := glblMeterProvider.Shutdown(ctx); err != nil {
+		log.Fatalf("failed to shutdown MeterProvider: %s", err)
+	}
+}
 
 func StartLoadBalancer() {
 	frontend, _ := zmq.NewSocket(zmq.ROUTER)
