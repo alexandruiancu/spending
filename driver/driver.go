51bd999c4e345df452282f7155607dc1e16a8a8b
Alex IANCU <alexandru.iancu@gmail.com>
2026-01-15 19:23:30 +0200

    feat: add the new calls

--- driver/driver.go
+++ driver/driver.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
+	//"context"
+	//"os"
+	//"os/signal"
+
+	"context"
+	"os"
+	"os/signal"
 	"spending/bldrec"
 	"spending/loader"
 )
 
 func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	//TODO: pass actual resource
+	err := loader.CreateMetricsPipeline(ctx)
+	if err != nil {
+		log.Fatalf("error creating metric pipeline: %v", err)
+	}
+	defer loader.ShutdownMetric(ctx)
+	fmt.Printf("Metrics pipeline created ...\n")
+	err = loader.CreateDebitInstrument()
+	if err != nil {
+		log.Fatalf("error creating debit instrument: %v", err)
+	}
+	fmt.Printf("Debit instrument created ...\n")
+
 	go bldrec.Process()
 	go loader.StartLoadBalancer()
 	select {}
